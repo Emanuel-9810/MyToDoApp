f4f6ea483d072eab671026bea533738b6044df65
Emanuel-9810 <60586959+Emanuel-9810@users.noreply.github.com>
2021-12-19 23:59:46 -0800

    Fixed Dashboard

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -4,6 +4,11 @@ import { Card, Button, Alert, Navbar, Nav, Container } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 import Box from "@mui/material/Box";
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 
 
@@ -65,21 +70,79 @@ export default function Dashboard() {
       </Navbar>
 
       <Box style={boxStyle}>
+        <h1 align="center">Welcome to your productivity profile</h1>
+        <br></br>
+        <h2 align="center">Click on the accordian below to view details on the website's pages</h2>
       <div
         className="d-flex align-items-center justify-content-center"
         style={{ minHeight: "100vh" }}
       >
         <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Card>
-            <Card.Body>
-              <h2 className="text-center mb-4">Profile</h2>
-              {error && <Alert variant="danger">{error}</Alert>}
-              <strong>Email: </strong> {currentUser.email}
-              <Link to="/update-profile" className="btn btn-success w-100 mt-3">
-                Update Email
-              </Link>
-            </Card.Body>
-          </Card>
+          <Accordion>
+            {/* Events */}
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>My Events</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                In this page, you can see your up to date google calendar, along with its events,
+                time of the event, location, and descriptive details.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          {/* Calendar */}
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography>My Calendar</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Create events that will directly be sent to and added to your google calendar, first you
+                would login with the google button, then from there you will be given a form to fill out with
+                the event details. This will then be saved onto your primary calendar.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          {/* Productivity */}
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography>Productivity</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                View resources to become more productive and overall become better with time management,
+                productivity, and even health.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          {/* Notes */}
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography>Notes</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                On this page, you can create to do notes and memos to yourself for a simple reminder that
+                you can look back on throughout your day.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
         </div>
       </div>
       </Box>
